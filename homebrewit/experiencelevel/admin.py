93766c67d3ac67ae60a5813a29e8a8491d015223
Patrick Carroll <patick@patrickomatic.com>
2010-12-05 12:33:09 -0500

    Models for experience level

---
+++ homebrewit/experiencelevel/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from homebrewit.experiencelevel.models import *
+
+admin.site.register(UserExperienceLevel)
+admin.site.register(ExperienceLevel)
--- homebrewit/experiencelevel/models.py
+++ homebrewit/experiencelevel/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ExperienceLevel(models.Model):
+	name = models.CharField(max_length=255)
+	img_url = models.FilePathField(path=settings.MEDIA_ROOT)
+
+	def __unicode__(self):
+		return self.name
+
+
+class UserExperienceLevel(models.Model):
+	user = models.ForeignKey(User)
+	experience_level = models.ForeignKey(ExperienceLevel)
+
+	def __unicode__(self):
+		return "%s: %s" % (self.user, self.experience_level)
+
+	def to_css(self):
+		# XXX print the CSS needed to show the experience level
+		return ""
