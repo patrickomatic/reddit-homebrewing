691d41489894c84f0163554a211623bf1df8e384
Patrick Carroll <patick@patrickomatic.com>
2010-12-05 14:42:07 -0500

    Added LOGIN_URL and LOGOUT_URL

--- homebrewit/settings.py
+++ homebrewit/settings.py
@@ -105,10 +105,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'homebrewit.contest',
     'homebrewit.experiencelevel',
     'homebrewit.signup',
@@ -116,3 +113,6 @@ INSTALLED_APPS = (
 )
 
 CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
+
+LOGIN_URL = '/login'
+LOGOUT_URL = '/logout'
