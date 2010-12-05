e9b0883a3bd05631f941fa4180561777312d6071
Patrick Carroll <patick@patrickomatic.com>
2010-12-05 14:44:13 -0500

    Remove some verbose comments

--- homebrewit/settings.py
+++ homebrewit/settings.py
@@ -59,17 +59,12 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media') 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = '/media/'
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Make this unique, and don't share it with anybody.
@@ -93,9 +88,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'homebrewit.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
 	os.path.join(PROJECT_PATH, 'templates')
 )
 
