fbed899439867b412846c2b0c720e38c5821b3ba
Patrick Carroll <patick@patrickomatic.com>
2011-01-17 15:28:08 -0500

    Pretty formatting

--- homebrewit/contest/urls.py
+++ homebrewit/contest/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('homebrewit.contest.views',
-		(r'^winner-styles.css$', 'winner_styles'),
-		(r'^register$', 'register'),
-		(r'^(?P<year>\d+)/styles$', 'styles'),
-		(r'^(?P<year>\d+)/$', 'contest_year'),
-		(r'^(?P<year>\d+)/entries/(?P<entry_id>\d+)$', 'entry'),
+		(r'^register$', 								'register'),
+		(r'^(?P<year>\d+)/styles$', 					'styles'),
+		(r'^(?P<year>\d+)/styles/(?P<style_id>\d+)$', 	'style'),
+		(r'^(?P<year>\d+)/entries/(?P<entry_id>\d+)$', 	'entry'),
+		(r'^(?P<year>\d+)/$', 							'contest_year'),
 )
