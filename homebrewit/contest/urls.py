32cf0fae83c23f5cb2bfb57915f38e943e0ad3c5
Patrick Carroll <patick@patrickomatic.com>
2011-01-20 21:02:40 -0500

    Contest year page working

--- homebrewit/contest/urls.py
+++ homebrewit/contest/urls.py
@@ -5,7 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('homebrewit.contest.views',
 		(r'^register$', 								'register'),
-		(r'^(?P<year>\d+)/styles$', 					'styles'),
 		(r'^(?P<year>\d+)/styles/(?P<style_id>\d+)$', 	'style'),
 		(r'^(?P<year>\d+)/entries/(?P<entry_id>\d+)$', 	'entry'),
 		(r'^(?P<year>\d+)/$', 							'contest_year'),
