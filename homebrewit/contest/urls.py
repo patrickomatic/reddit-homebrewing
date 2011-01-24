4a5f6fc0936fdf09a326fc7be818c4260f97a13a
Patrick Carroll <patick@patrickomatic.com>
2011-01-24 18:58:31 -0500

    Map the winner-styles.css view

--- homebrewit/contest/urls.py
+++ homebrewit/contest/urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('homebrewit.contest.views',
-		(r'^register$', 								'register'),
-		(r'^(?P<year>\d+)/styles/(?P<style_id>\d+)$', 	'style'),
-		(r'^(?P<year>\d+)/entries/(?P<entry_id>\d+)$', 	'entry'),
-		(r'^(?P<year>\d+)/$', 							'contest_year'),
+		(r'^winner-styles.css$', 'winner_styles'),
+		(r'^register$', 'register'),
+		(r'^(?P<year>\d+)/styles/(?P<style_id>\d+)/entries/(?P<entry_id>\d+)$', 'entry'),
+		(r'^(?P<year>\d+)/styles/(?P<style_id>\d+)$', 'style'),
+		(r'^(?P<year>\d+)/$','contest_year'),
 )
