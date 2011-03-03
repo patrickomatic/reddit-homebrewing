2bd4be8778b24a125b2bd092ad9a1a5340deb481
Patrick Carroll <patick@patrickomatic.com>
2011-03-02 19:36:47 -0500

    Moved contest judging functionality into a model manager method

--- homebrewit/contest/management/commands/judgecontest.py
+++ homebrewit/contest/management/commands/judgecontest.py
@@ -9,29 +9,4 @@ class JudgeContestCommand(BaseCommand):
 
 	def handle(self, *args, **options):
 		for year in args:
-			year = int(year)
-
-			# calculate the score of each entry for the year
-			for entry in Entry.objects.filter(style__contest_year__contest_year=year):
-				total, num = 0, 0
-
-				# get the average of each judge who judged this entry
-				results = JudgingResult.objects.filter(entry=entry)
-
-				if results:
-					for result in results:
-						num += 1
-						total += result.overall_rating()
-
-					entry.score = total / num
-					entry.save()
-
-
-			# now for each style, assign winners
-			for style in BeerStyle.objects.filter(contest_year__contest_year=year):
-				place = 1
-				for entry in Entry.objects.get_top_3(style):
-					entry.winner = True
-					entry.rank = place
-					entry.save()
-					place += 1
+			Entry.objects.judge_entries(int(year))
--- homebrewit/contest/models.py
+++ homebrewit/contest/models.py
@@ -52,6 +52,33 @@ class EntryManager(models.Manager):
 		return set([entry.user for entry in Entry.objects.filter(winner=True, 
 										style__contest_year__contest_year=contest_year)])
 
+	
+	def judge_entries(self, year):
+		# calculate the score of each entry for the year
+		for entry in Entry.objects.filter(style__contest_year__contest_year=year):
+			total, num = 0, 0
+
+			# get the average of each judge who judged this entry
+			results = JudgingResult.objects.filter(entry=entry)
+
+			if results:
+				for result in results:
+					num += 1
+					total += result.overall_rating()
+
+				entry.score = total / num
+				entry.save()
+
+
+		# now for each style, assign winners
+		for style in BeerStyle.objects.filter(contest_year__contest_year=year):
+			place = 1
+			for entry in Entry.objects.get_top_3(style):
+				entry.winner = True
+				entry.rank = place
+				entry.save()
+				place += 1
+
 
 class Entry(models.Model):
 	beer_name = models.CharField(max_length=255, null=True, blank=True)
