0f41cd54a1e53859f8f0ef466730b775479ca07c
Patrick Carroll <patick@patrickomatic.com>
2011-03-07 19:31:24 -0500

    Have to allow most of these fields to be None
    Given that it's created when it's first accessed it may have to be None

--- homebrewit/signup/models.py
+++ homebrewit/signup/models.py
@@ -5,17 +5,19 @@ from django.db.models.signals import post_save
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(User)
-	email = models.EmailField()
-	address_1 = models.CharField(max_length=255)
+	address_1 = models.CharField(max_length=255, null=True, blank=True)
 	address_2 = models.CharField(max_length=255, null=True, blank=True)
-	city = models.CharField(max_length=255)
-	state = models.CharField(max_length=255)
-	country = models.CharField(max_length=255)
-	zip_code = models.PositiveIntegerField()
+	city = models.CharField(max_length=255, null=True, blank=True)
+	state = models.CharField(max_length=255, null=True, blank=True)
+	country = models.CharField(max_length=255, null=True, blank=True)
+	zip_code = models.CharField(max_length=255, null=True, blank=True)
 
 	def __unicode__(self):
 		return self.user.username
 
+	def is_complete(self):
+		# check some fields that shouldn't be null
+		return None in (self.user, self.user.email, self.country)
 
 
 def create_user_profile(sender, instance, created, **kwargs):
