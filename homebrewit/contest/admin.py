681f5f20ab1b7560b426b0d1648c4d9bb39dd831
Patrick Carroll <patick@patrickomatic.com>
2011-03-28 22:02:09 -0400

    Add ContestYear

--- homebrewit/contest/admin.py
+++ homebrewit/contest/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from homebrewit.contest.models import *
 
 admin.site.register(BeerStyle)
+admin.site.register(ContestYear)
 admin.site.register(Entry)
 admin.site.register(JudgingResult)
