4344e8a502878d6e9e28fd6ed55e75981e03d95f
Patrick Carroll <patick@patrickomatic.com>
2011-09-06 18:28:34 -0400

    Textarea widgets on the judging results page

--- homebrewit/contest/admin.py
+++ homebrewit/contest/admin.py
@@ -1,7 +1,25 @@
+from django import forms
 from django.contrib import admin
+from django.db import models
+
 from homebrewit.contest.models import *
+
+
+class JudgingResultAdminForm(forms.ModelForm):
+	class Meta:
+		model = JudgingResult
+		widgets = {
+				'aroma_description': forms.Textarea(),
+				'appearance_description': forms.Textarea(),
+				'mouthfeel_description': forms.Textarea(),
+				'flavor_description': forms.Textarea(),
+				'overall_impression_description': forms.Textarea(),
+		}
+
+class JudgingResultAdmin(admin.ModelAdmin):
+	form = JudgingResultAdminForm
 
 admin.site.register(BeerStyle)
 admin.site.register(ContestYear)
 admin.site.register(Entry)
-admin.site.register(JudgingResult)
+admin.site.register(JudgingResult, JudgingResultAdmin)
