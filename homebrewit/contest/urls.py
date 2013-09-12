88d094423952edbce8868dd07a21bdd26f4b04b2
Patrick Carroll <patrick@patrickomatic.com>
2013-09-11 20:55:59 -0400

    Tabs not spaces

--- homebrewit/contest/urls.py
+++ homebrewit/contest/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('homebrewit.contest.views',
 		(r'^(?P<year>\d+)/styles/(?P<style_id>\d+)/entries/(?P<entry_id>\d+)$', 'entry'),
 		(r'^(?P<year>\d+)/styles/(?P<style_id>\d+)$', 'style'),
 		(r'^(?P<year>\d+)/$', 'contest_year'),
-        (r'^judgeentry$', 'entry_judging_form'),
+		(r'^judgeentry$', 'entry_judging_form'),
 )
