a6cfc42388e7b172bbb30b946996fe9ccc921b0f
Patrick Carroll <patrick@patrickomatic.com>
2014-04-12 16:33:32 -0400

    Import from a different location

--- homebrewit/wsgi.py
+++ homebrewit/wsgi.py
@@ -10,5 +10,5 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homebrewit.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from django.core.handlers.wsgi 
+application = django.core.handles.wsgi.WSGIHandler()
