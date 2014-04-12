5389d973190890b2569cf9e7eaf08a1afb0e06e0
Patrick Carroll <patrick@patrickomatic.com>
2014-04-12 17:06:43 -0400

    Whoops got key wrong

--- homebrewit/settings.py
+++ homebrewit/settings.py
@@ -160,7 +160,7 @@ if False:
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default=os.environ['HEROKU_POSTGRES_BLACK_URL'])
+DATABASES['default'] =  dj_database_url.config(default=os.environ['HEROKU_POSTGRESQL_BLACK_URL'])
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
