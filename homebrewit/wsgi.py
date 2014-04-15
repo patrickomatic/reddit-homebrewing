dcc96f54b0b693cad37caa1fb7b0ef9ffa0a8d41
Patrick Carroll <patrick@patrickomatic.com>
2014-04-14 20:49:12 -0400

    Remove debugging

--- homebrewit/wsgi.py
+++ homebrewit/wsgi.py
@@ -7,7 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-print "In wsgi.py"
 import django.core.handlers.wsgi 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
@@ -17,5 +16,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homebrewit.settings")
 application = django.core.handlers.wsgi.WSGIHandler()
 
 application = Cling(get_wsgi_application())
-
-print "app start"
