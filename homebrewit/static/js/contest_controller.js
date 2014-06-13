35049fdb0344143ace43cb8996b25727e8d18099
Patrick Carroll <patrick@patrickomatic.com>
2014-06-12 20:32:26 -0400

    Conditional for whether a menu item is clickable

--- homebrewit/static/js/contest_controller.js
+++ homebrewit/static/js/contest_controller.js
@@ -19,10 +19,10 @@ contestApp.controller('ContestSignupCtrl', function($scope, $http) {
         $scope.status.isopen = !$scope.status.isopen;
     }
 
-	$http.get('/contests/2011/beer_styles').success(function(data) {
-		$scope.styles = data;
-		$scope.style = data[0];
-	});
+    $http.get('/contests/2011/beer_styles').success(function(data) {
+        $scope.styles = data;
+        $scope.style = data[0];
+    });
 
     $scope.registerForContest = function($http) {
         $http.post('/contests/' + $scope.contestYear + '/beer_styles/' + $scope.style);
