a60d57aace073f7d9f318ab238b9439ecaaa67c4
Patrick Carroll <patrick@patrickomatic.com>
2014-09-16 21:56:51 -0400

    Use named urls

--- homebrewit/profile/urls.py
+++ homebrewit/profile/urls.py
@@ -6,7 +6,7 @@ from homebrewit.profile import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'edit$', 'edit_profile'),
-    (r'password$', 'change_password'),
+    (r'edit$', 'edit_profile', name='edit_profile'),
+    (r'password$', 'change_password', name='change_password'),
     url(r'^(?P<username>[\w_-]+)|$', views.ProfileView.as_view(), name='profile'),
 )
